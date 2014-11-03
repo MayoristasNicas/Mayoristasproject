25e3845c5ea0acc5459af74a4c25d736b08e86af
farolito20 <farolito20@hotmail.com>
2014-11-02 22:25:49 -0600

    Merge origin/master
    Conflicts:
    	MayoristasNicas_1stVersion/web/index.jsp
    	MayoristasNicas_1stVersion/web/viewProducts_.jsp

--- MayoristasNicas_1stVersion/src/java/helpers/EmailValidator.java
+++ MayoristasNicas_1stVersion/src/java/helpers/EmailValidator.java
@@ -3,8 +3,16 @@
  * and open the template in the editor.
  */
 package helpers;
+import java.util.Properties;
 import java.util.Scanner;
 import java.util.regex.*;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 /**
  *
@@ -29,8 +37,49 @@ public class EmailValidator {
     public boolean validate(final String hex) {
 
         matcher = pattern.matcher(hex);
-        return matcher.matches();
+        
+        //Send email
+        if(matcher.matches()){
+            return this.sendEmail(hex);
+        }
+        return false;
 
+    }
+    
+    public boolean sendEmail(String email){
+        final String username = "mayoristasnicas.soporte@gmail.com";
+        final String password = "mayoristas";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(email));
+            message.setSubject("Confirmación cuenta MayoristasNicas");
+            message.setText("Estimado Usuario,"
+                    + "\n\n Gracias por suscribirse a MayoristasNicas");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+            return true;
+        } catch (MessagingException e) {
+            return false;
+        }
     }
           
           /*
